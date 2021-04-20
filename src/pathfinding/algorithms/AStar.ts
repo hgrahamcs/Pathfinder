a6a895b215dacbb51c519f13e796589ac49d09ac
Ltprichard2019@gmail.com <58538077+JosephPrichard@users.noreply.github.com>
2021-04-19 21:19:14 -0500

    Refactoring + Optimization
    Refactored pathfinding algorithms
    Removed redundant expansions on nodes already in closedSet for A* and BiA*

--- src/pathfinding/algorithms/AStar.ts
+++ src/pathfinding/algorithms/AStar.ts
@@ -1,4 +1,4 @@
-import {HashTable, stringify} from '../structures/Hash';
+import {HashSet, HashTable, stringify} from '../structures/Hash';
 import Heap from '../structures/Heap';
 import Pathfinder, {reconstructPath} from './Pathfinder';
 import {Point, Tile} from '../core/Components';
@@ -9,15 +9,15 @@ import {euclidean, HeuristicFunc} from './Heuristics';
 class AStarPathfinder extends Pathfinder
 {
     private readonly heuristic: HeuristicFunc = (a: Point, b: Point) => euclidean(a,b);
-    private readonly isNewFBetter: (newF: number, oldF: number) => boolean;
+    private readonly isNewScoreBetter: (newF: number, oldF: number) => boolean;
 
     constructor(navigator: Navigator, func?: HeuristicFunc, canRediscover?: boolean) {
         super(navigator);
         if(func !== undefined) {
             this.heuristic = func;
         }
-        this.isNewFBetter = canRediscover === undefined || canRediscover ?
-            (newF: number, oldF: number) => newF < oldF :
+        this.isNewScoreBetter = canRediscover === undefined || canRediscover ?
+            (newScore: number, oldScore: number) => newScore < oldScore :
             () => false;
     }
 
@@ -34,34 +34,41 @@ class AStarPathfinder extends Pathfinder
     findPath(initial: Point, goal: Point): Tile[] {
         this.clearRecentSearch();
         const grid = this.navigator.getGrid();
-        const openSet = new Heap<AStarNode>(
+        const openFrontier = new Heap<AStarNode>(
             (a, b) => a.f() < b.f()
         );
-        const closedSet = new HashTable<number>();
+        const closedSet = new HashSet();
+        const openSet = new HashTable<number>();
         const root = new AStarNode(
             grid.get(initial), 0, 0
         );
-        openSet.push(root);
-        closedSet.add(stringify(initial), root.f());
-        while (!openSet.isEmpty()) {
-            const currentNode = openSet.pop();
-            this.addRecent(currentNode);
+        openFrontier.push(root);
+        openSet.add(stringify(initial), root.f());
+        while (!openFrontier.isEmpty()) {
+            const currentNode = openFrontier.pop();
             const currentPoint = currentNode.tile.point;
+            const currentKey = stringify(currentPoint);
+            openSet.remove(currentKey);
+            closedSet.add(currentKey);
+            this.addRecent(currentNode);
             if (this.navigator.equals(currentPoint, goal)) {
                 return reconstructPath(currentNode);
             }
             for (const neighbor of this.navigator.neighbors(currentPoint)) {
                 const neighborPoint = neighbor.point;
                 const neighborKey = stringify(neighborPoint);
+                if(closedSet.has(neighborKey)) {
+                    continue;
+                }
                 const g = currentNode.g + this.stepCost(currentPoint, neighborPoint);
                 const f = g + this.heuristic(neighborPoint, goal);
-                if (!closedSet.has(neighborKey) || this.isNewFBetter(g, closedSet.get(neighborKey)!)) {
+                if (!openSet.has(neighborKey) || this.isNewScoreBetter(g, openSet.get(neighborKey)!)) {
                     const neighborNode = new AStarNode(
                         neighbor, g, f
                     );
                     currentNode.addChild(neighborNode);
-                    openSet.push(neighborNode);
-                    closedSet.add(neighborKey, neighborNode.g);
+                    openFrontier.push(neighborNode);
+                    openSet.add(neighborKey, neighborNode.g);
                 }
             }
         }
